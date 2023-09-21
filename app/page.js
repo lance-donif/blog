c0d8a7e1b3df25b4dd88a8c4eec86b8bf99dff5f
lance <lance@lance.com>
2023-09-21 16:53:44 +0800

    1

--- app/page.js
+++ app/page.js
@@ -1,95 +1,113 @@
+'use client'
+import React, {useEffect, useRef, useState} from 'react';
+import {useEditor, EditorContent} from '@tiptap/react'
+import StarterKit from '@tiptap/starter-kit'
+import css from './page.module.css'
 import Image from 'next/image'
-import styles from './page.module.css'
+import TextAlign from '@tiptap/extension-text-align'
+import TiptapImage from "./tiptapImage";
 
 export default function Home() {
-  return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>app/page.js</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
+    const divRef = useRef(null);
+    const [imageSize,setImageSize] = useState({width:100,height:100})
+    useEffect(() => {
 
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+        if (divRef.current) {
+            const parentDiv = divRef.current.parentNode;
+            const parentWidth = parentDiv.offsetWidth;
+            const parentHeight = parentDiv.offsetHeight;
+            console.log("父元素的宽度：" + parentWidth);
+            console.log("父元素的高度：" + parentHeight);
+            // setImageSize({width:parentWidth,height:parentHeight})
+        }
+    }, [divRef]);
+    // 定义编辑器一些属性
+    const [tiptapEditor, setTiptapEditor] = useState();
+    // tiptap配置
+    const editor = useEditor({
+        editorProps: {
+            attributes: {
+                class: css.editor,
+            },
+        },
+        extensions: [
+            StarterKit, TextAlign.configure({
+                types: ['heading', 'paragraph'],
+            }), TiptapImage
+        ],
+        content: '<p>Hello World! 🌎️</p>' +
+            '<tiptap-image ref={divRef}  src="https://source.unsplash.com/8xznAGy4HcY/800x400"></tiptap-image>'
+    })
+    const svgMap = [
+        {
+            name: '居中',
+            src: '/editor/align-center.svg',
+            button: () => editor.chain().focus().setTextAlign('center').run(),
+        },
+        {
+            name: '居左',
+            src: '/editor/align-left.svg',
+            button: () => editor.chain().focus().setTextAlign('left').run(),
+        },
+        {
+            name: '居右',
+            src: '/editor/align-right.svg',
+            button: () => editor.chain().focus().setTextAlign('right').run(),
+        },
+        {
+            name: '加粗',
+            src: '/editor/bold.svg',
+            button: () => editor.chain().focus().toggleBold().run(),
+        },
+    ]
+    return (
+        <>
+            <div className={css.body}>
+                <div style={{
+                    width: '700px', height: '500px',
+                    backgroundColor: 'pink',
+                    borderRadius: '10px',
+                    display: 'flex',
+                    flexDirection: 'column'
+                }}>
+                    <div style={{
+                        borderRadius: '10px 10px 0 0', backgroundColor: '#fff',
+                    }}>
+                        <ul style={{
+                            display: 'flex',
+                            width: '100%',
+                            padding: '10px',
+                        }}>
+                            {svgMap.map((svg, index) => {
+                                return <li key={index}
+                                           onClick={svg.button}
+                                           style={{
+                                               display: 'flex',
+                                               flexDirection: 'row-reverse',
+                                               justifyContent: 'center',
+                                               alignItems: 'center',
+                                               border: '2px solid black',
+                                               marginRight: '6px'
+                                           }}>
+                                    <div
+                                        style={{
+                                            width: '20px',
+                                            height: "20px",
+                                            backgroundImage: `url('${svg.src}`,
+                                        }}
+                                        title={svg.name}
+                                    >
 
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
+                                    </div>
+                                </li>
+                            })}
+                        </ul>
+                    </div>
+                    <EditorContent
+                        editor={editor}/>
+                </div>
+            </div>
+        </>
+    )
+};
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore the Next.js 13 playground.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
-}
--- next.config.js
+++ next.config.js
@@ -1,4 +1,12 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
-
+const nextConfig = {images: {
+        remotePatterns: [
+            {
+                protocol: 'https',
+                hostname: 'source.unsplash.com',
+                port: '',
+                pathname: '/**',
+            },
+        ],
+    },}
 module.exports = nextConfig
