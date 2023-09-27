98935bfe2a93b7d2d28a1b51fdf2ffffd16d6553
lance <lance@lance.com>
2023-09-27 20:55:11 +0800

    update

--- app/api/blog/[id]/route.js
+++ app/api/blog/[id]/route.js
@@ -3,10 +3,11 @@ import process from "next/dist/build/webpack/loaders/resolve-url-loader/lib/post
 
 export async function GET(req,{params}) {
     const url1 = process.env.NEXT_PUBLIC_url
+    const time = process.env.NEXT_PUBLIC_timeNumber
 
 
     const url = `${url1}/blog/${params.id}`;
-    const res = await fetch(url,{ next: { revalidate: 3600} } )
+    const res = await fetch(url,{ next: { revalidate: time} } )
     const data = await res.json()
     return NextResponse.json(data)
 
--- app/api/route.js
+++ app/api/route.js
@@ -3,9 +3,10 @@ import process from "next/dist/build/webpack/loaders/resolve-url-loader/lib/post
 
 export async function GET() {
     const url = process.env.NEXT_PUBLIC_url
+    const time = process.env.NEXT_PUBLIC_timeNumber
 
     // const url = "http://gz.lancevps.top:9999"
-    const res = await fetch(`${url}`, {next: {revalidate: 3600}})
+    const res = await fetch(`${url}`, {next: {revalidate: time}})
     const result = await res.json()
     return NextResponse.json(result)
 }
