323d241deca23b6c57f8a8ae71a359744795cffa
lance <lance@lance.com>
2023-09-27 20:46:07 +0800

    update

--- app/api/route.js
+++ app/api/route.js
@@ -5,7 +5,7 @@ export async function GET() {
     const url = process.env.NEXT_PUBLIC_url
 
     // const url = "http://gz.lancevps.top:9999"
-    const res = await fetch(`${url}`, {next: {revalidate: 0}})
+    const res = await fetch(`${url}`, {next: {revalidate: 3600}})
     const result = await res.json()
     return NextResponse.json(result)
 }
